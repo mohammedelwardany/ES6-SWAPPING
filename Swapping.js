ad52b0b98637dba4e638e9cb64226865718f3f56
mohammedelwardany <78797469+mohammedelwardany@users.noreply.github.com>
2022-02-13 23:32:26 +0200

    Add files via upload

---
+++ Swapping.js
@@ -0,0 +1,72 @@
+//==============================================================1st task====================================================//
+// let a = 15;
+// let b = 10;
+
+
+// [a, b] = [b, a];
+// console.log(a);
+// console.log(b);
+
+/////////////////////////////
+
+// temp = a;
+// a = b;
+// b = temp;
+// console.log(a);
+// console.log(b);
+
+
+//==============================================================2nd task====================================================//
+
+// const arr = [13, 35, 23, 18, 1, 100];
+
+// function maxmin(...args) {
+
+//     let maxi = Math.max(...args),
+//         mini = Math.min(...args);
+
+//     return {
+//         maxi,
+//         mini
+//     };
+// }
+// let maximin = maxmin(...arr);
+// let maximinrest = maxmin(3, 5, 12, 14, 10);
+// console.log(maximin.maxi);
+// console.log(maximin.mini);
+// console.log(maximinrest.maxi);
+// console.log(maximinrest.mini);
+
+
+//==============================================================3rd task====================================================//
+// // 1//
+
+// var fruits = ["apple", "strawberry", "banana", "orange", "mango"];
+
+// checkstring = fruits.every(i => (typeof i === "string"));
+// console.log(checkstring)
+
+
+// 2//
+
+// var checka = fruits.some(word => word.charAt(0) == "a");
+// console.log(checka);
+
+
+
+// 3//
+
+// const checkbs = fruits.filter(word => word.charAt(0) == "b" || word.charAt(0) == "s");
+// console.log(checkbs);
+
+// // 4//
+
+// var newarr = fruits.map(function(el) {
+
+//     return "i like " + el;
+// })
+// console.log(newarr);
+
+// // 5//
+
+// newarr.forEach(element => console.log(element));
